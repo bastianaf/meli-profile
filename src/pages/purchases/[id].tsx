9c6e5220d61687b886b12e6b6bbab2415c918b93
bastianaf <bastian.avila@pucv.cl>
2022-08-08 04:47:35 -0400

    :sparkles: fetch purchase shipment/transaction data

--- src/pages/purchases/[id].tsx
+++ src/pages/purchases/[id].tsx
@@ -16,12 +16,18 @@ import {
 
 const UserPurchaseDetail: NextPageWithLayout = () => {
   
+  const INITIAL_STATE: PurchaseDetail  = ({} as any) as PurchaseDetail
+  const [ purchaseDetailState, setPurchaseDetail ] = useState(INITIAL_STATE)
+
+  const SHIPMENT_INITIAL_STATE: any  = ({} as any) as any
+  const [ shipmentState, setShipmentState ] = useState(SHIPMENT_INITIAL_STATE)
+
+  const TRANSACTION_INITIAL_STATE: any  = ({} as any) as any
+  const [ transactionState, setTransactionState ] = useState(TRANSACTION_INITIAL_STATE)
+
   const { userProfile } = useUser()
   const { userPurchases } = usePurchase()
 
-  const INITIAL_STATE: PurchaseDetail = ({} as any) as PurchaseDetail 
-  const [ purchaseDetailState, setPurchaseDetail ] = useState(INITIAL_STATE)
-  
   const router = useRouter()
   const { id } = router.query
 
@@ -41,26 +47,62 @@ const UserPurchaseDetail: NextPageWithLayout = () => {
     }
   } 
 
-  
+  const fetchShipmnetDetail = async (shipmentId: number | undefined) => {
+    if(shipmentId){
+      await fetch(`http://localhost:4000/shipment/${shipmentId}`, {
+        method: "GET",
+      }).then( async (res) =>{
+        if (res.ok) {
+          const result = await (res.json())
+          console.log("SHIPMENT", result);
+          setShipmentState(result)
+        }
+      }).catch((error) => {
+        console.error(error)
+      })
+    }
+  } 
+
+  const fetchTransactionDetail = async (paymentId: number | undefined) => {
+    if(paymentId){
+      await fetch(`http://localhost:4000/payment/${paymentId}`, {
+        method: "GET",
+      }).then( async (res) =>{
+        if (res.ok) {
+          const result = await (res.json())
+          console.log("TRANSACTION", result);
+          setTransactionState(result)
+        }
+      }).catch((error) => {
+        console.error(error)
+      })
+    }
+  } 
 
   useEffect( () => {
-
     console.log("use effect | current state: ", purchaseDetailState)
-
-    setPurchaseDetail ((state): PurchaseDetail => {
-      return userPurchases?.find( purchase => purchase.id_compra === id) || state
+    setPurchaseDetail ((state): any => {
+      return { ...state, purchase: userPurchases?.find( purchase => purchase.id_compra === id) || null}
     })
-
     if(!purchaseDetailState.id_compra){
       console.log("use effect call fetch purchase detail | current state: ", purchaseDetailState)
       fetchPurchaseDetail()
-    }else {
-
     }
-
   }, [id, userProfile]);
 
+  useEffect( () => {
+    if(purchaseDetailState.id_envio){
+      console.log("use effect call fetch SHIPMENT / TRANSACTION detail | current state: ", purchaseDetailState)
 
+      if(!shipmentState.id_envio){
+        fetchShipmnetDetail(purchaseDetailState.id_envio)
+      }
+
+      if(!transactionState.id_transaccion){
+        fetchTransactionDetail(purchaseDetailState.id_transaccion)
+      }
+    }
+  }, [purchaseDetailState]);
 
   if(!purchaseDetailState.id_compra) {
     return (
@@ -73,9 +115,20 @@ const UserPurchaseDetail: NextPageWithLayout = () => {
     )
   } else {
     return (
-      <p>
-        {purchaseDetailState?.id_compra || 'a'} {purchaseDetailState?.id_envio}
-      </p>
+      <>
+        <p>
+          {purchaseDetailState?.id_compra || 'a'} {purchaseDetailState?.id_envio}
+        </p>
+      
+        <p>
+          {shipmentState.estado ?  shipmentState.estado  : 'cargando datos de envio...'}
+        </p>
+
+        <p>
+          {transactionState.estado ?  transactionState.estado  : 'cargando datos de la transaccion...'}
+        </p>
+        
+      </>
     )
   }
 }
